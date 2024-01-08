8d7e72c65a65e48c8c402cb81ef7c03cfbfa29b9
Gabriel Tadeu <gabrieltadeums11@outlook.com>
2024-01-08 14:09:27 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,144 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue;
+
+// Funções
+const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'; // aspas simples, pois ja tem duplas dentro
+  todo.appendChild(doneBtn);
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-todo");
+  removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(removeBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = ""; // apagar o input assim que der submit
+  todoInput.focus(); // assim que der submit, o foco volta para o input
+};
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  });
+};
+
+const filterTodos = (filterValue) => {
+  const todos = document.querySelectorAll(".todo");
+
+  switch (filterValue) {
+    case "all":
+      todos.forEach((todo) => (todo.style.display = "flex"));
+
+      break;
+
+    case "done":
+      todos.forEach((todo) =>
+        todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    case "todo":
+      todos.forEach((todo) =>
+        !todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    default:
+      break;
+  }
+};
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = todoInput.value;
+
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div"); // seleciono a div pai mais proxima do elemento que me refiro
+  let todoTitle;
+
+  if (parentEl && parentEl.querySelector("h3")) {
+    todoTitle = parentEl.querySelector("h3").innerText;
+  }
+
+  if (targetEl.classList.contains("finish-todo")) {
+    parentEl.classList.toggle("done");
+  }
+
+  if (targetEl.classList.contains("remove-todo")) {
+    parentEl.remove();
+  }
+
+  if (targetEl.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+});
+
+filterBtn.addEventListener("change", (e) => {
+  const filterValue = e.target.value;
+  filterTodos(filterValue);
+});
